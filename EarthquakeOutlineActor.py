c63ccb3a36200ad3b95c20f10c3f8557aa287f1a
JudithNeukamm <judith.neukamm@gmx.net>
2014-10-08 13:40:27 +0200

    set bounds of outlineSource

--- EarthquakeOutlineActor.py
+++ EarthquakeOutlineActor.py
@@ -3,44 +3,30 @@ from vtk import *
 
 class EarthquakeOutlineActor(vtkActor):
 
-    def __init__(self, data):
-        self.filter = None
+    def __init__(self, xmin=0, xmax=0, ymin=0, ymax=0, zmin=0, zmax=0):
         self.mapper = None
-
-        self.init_filter(data)
+        self.source = None
+        
+        self.xmin = xmin
+        self.xmax = xmax
+        self.ymin = ymin
+        self.ymax = ymax
+        self.zmin = zmin
+        self.zmax = zmax
+        
         self.init_mapper()
 
         self.SetMapper(self.mapper)
         self.GetProperty().SetDiffuseColor(0.8, 0.8, 0.8)
         self.GetProperty().SetLineWidth(2.0)
+ 
 
-    def init_filter(self, data):
-        self.filter = vtk.vtkOutlineFilter()
-        self.filter.SetInput(data)
-        
-        
-        
-        jpg_reader = vtk.vtkJPEGReader()
-        jpg_reader.SetFileName('map.jpg')
-        
-        #ia = vtk.vtkImageActor()
-        #ia.GetMapper().SetInputConnection(jpg_reader.GetOutputPort())
-        
-        
-        picker = vtk.vtkCellPicker()
-        picker.SetTolerance(0.005)
-
-        planeWidgetX = vtk.vtkImagePlaneWidget()
-        planeWidgetX.DisplayTextOn()
-        planeWidgetX.SetInput(jpg_reader.GetOutput())
-        planeWidgetX.SetPlaneOrientationToXAxes()
-        planeWidgetX.SetSliceIndex(32)
-        planeWidgetX.SetPicker(picker)
-       
-        prop1 = planeWidgetX.GetPlaneProperty()
-        prop1.SetColor(1, 0, 0)
+    def init_source(self):
+        self.source = vtk.vtkOutlineSource()
+        self.source.SetBonds(self.xmin, self.xmax, self.ymin, self.ymax, self.zmin, self.zmax)
+ 
 
     def init_mapper(self):
         self.mapper = vtk.vtkPolyDataMapper()
-        self.mapper.SetInput(self.filter.GetOutput())
+        self.mapper.SetInput(self.source)
         
